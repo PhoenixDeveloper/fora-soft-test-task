3121c8947b9ca1defe50b139ffd8e71a3f54b160
Mikhail Belenko <mefisto40000x@gmail.com>
2020-12-09 10:01:25 +0300

    Create project

---
+++ ForaSoftTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForaSoftTestTask
+//
+//  Created by Михаил Беленко on 09.12.2020.
+//  Copyright © 2020 MOB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
